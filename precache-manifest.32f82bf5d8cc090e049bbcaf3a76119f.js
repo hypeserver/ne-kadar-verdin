e48e5e74276efdf828b63b7045dd7b99bf0eea8b
Kerem Sevencan <ciukerem@gmail.com>
2019-03-02 13:03:54 +0100

    Updates

---
+++ precache-manifest.32f82bf5d8cc090e049bbcaf3a76119f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "94f4f0918a13f367eeeb",
+    "url": "/ne-kadar-verdin/static/js/runtime~main.94f4f091.js"
+  },
+  {
+    "revision": "9561df0deef18d2fd07f",
+    "url": "/ne-kadar-verdin/static/js/main.9561df0d.chunk.js"
+  },
+  {
+    "revision": "78285bcbfba8e3a0347a",
+    "url": "/ne-kadar-verdin/static/js/2.78285bcb.chunk.js"
+  },
+  {
+    "revision": "9561df0deef18d2fd07f",
+    "url": "/ne-kadar-verdin/static/css/main.d39dc7f0.chunk.css"
+  },
+  {
+    "revision": "1f377ea0fc60e5e1db4d2c008685eb92",
+    "url": "/ne-kadar-verdin/index.html"
+  }
+];
