97fb0fd800c73166ce7b37c5ef73b12a03e3f91e
Kerem Sevencan <kerem@ritmix.org>
2020-03-30 23:39:52 +0300

    Updates

---
+++ precache-manifest.54a01373d25e2d93aab3b5b8d65c17da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0b34479b9f3e997bce1a8187812e82b",
+    "url": "/ne-kadar-verdin/index.html"
+  },
+  {
+    "revision": "61c653d577f4aab32b21",
+    "url": "/ne-kadar-verdin/static/css/main.4dcb014e.chunk.css"
+  },
+  {
+    "revision": "8b9fdd912691f7c5efe5",
+    "url": "/ne-kadar-verdin/static/js/2.2a738b12.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ne-kadar-verdin/static/js/2.2a738b12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61c653d577f4aab32b21",
+    "url": "/ne-kadar-verdin/static/js/main.d2083393.chunk.js"
+  },
+  {
+    "revision": "49e1808495313e83eab5",
+    "url": "/ne-kadar-verdin/static/js/runtime-main.32a86496.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ne-kadar-verdin/precache-manifest.b5a818e12f32912b729c6272b8c35bb4.js"
+  "/ne-kadar-verdin/precache-manifest.54a01373d25e2d93aab3b5b8d65c17da.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/ne-kadar-verdin/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ne-kadar-verdin/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
